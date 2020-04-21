317042b187a794394fb06268129760f919b3b87e
samuray90 <samuray_90@yahoo.com>
2020-04-21 17:10:46 +0100

    testing.jsx

---
+++ src/component/button/button.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Button = ({ onClick }) => {
+    return (
+        <button
+            onClick={onClick}
+            data-testid="nice-button">
+            hi
+        </button>
+    )
+}
+
+export { Button }
